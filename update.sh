1cb8e4aa9f408ba89815bb2415683a612ff3a7a0
RomLay <tuan180220@icloud.com>
2025-07-24 23:54:08 +0700

    test

--- update.sh
+++ update.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
+
 function building() {
   echo "[" > all.pkgs
-if [[ -e compatity.txt ]]; then
-    compatity=$(cat compatity.txt)
-fi
+  
+  # Kiểm tra thư mục debs/
+  if [ ! -d "debs" ]; then
+    echo "Error: debs directory not found!"
+    exit 1
+  fi
 
-for i in debs/*.deb
-do
-   debInfo=`dpkg -f $i`
-   pkg=`echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`
+  if [[ -e compatity.txt ]]; then
+    compatity=$(cat compatity.txt)
+  fi
+
+  # Kiểm tra có file .deb nào không
+  if [ ! "$(ls -A debs/*.deb 2>/dev/null)" ]; then
+    echo "Error: No .deb files found in debs directory!"
+    exit 1
+  fi
+
+  for i in debs/*.deb
+  do
+    if [ ! -f "$i" ]; then
+      continue
+    fi
+    
+    debInfo=`dpkg -f $i`
+    if [ $? -ne 0 ]; then
+      echo "Error reading $i"
+      continue
+    fi
+    
+    pkg=`echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`
 
    section=`echo "$debInfo" | grep "Section: " | cut -c 10- | tr -d "\n\r"`
    section="${section//'"'/\\\"}"
@@ -45,17 +68,57 @@ do
      echo "$pkg $tmp" >> compatity.txt;
   fi
 done
-
-echo "{}]" >> all.pkgs
+  
+  echo "{}]" >> all.pkgs
 }
+
 echo "------------------"
 echo "Building Packages...."
+if ! command -v apt-ftparchive &> /dev/null; then
+    echo "Error: apt-ftparchive not found!"
+    exit 1
+fi
+
+# Tạo Packages
 apt-ftparchive packages ./debs > ./Packages;
-#sed -i -e '/^SHA/d' ./Packages;
+
+# Tạo các phiên bản nén
 bzip2 -c9k ./Packages > ./Packages.bz2;
+gzip -c9k ./Packages > ./Packages.gz;
+
 echo "------------------"
 echo "Building Release...."
-printf "Origin: romlayvn Repo\nLabel: iPhone/iPad-Jailbreak\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitectures: iphoneos-arm iphoneos-arm64 iphoneos-arm64e\nComponents: main\nDescription: Apple-Support\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+# Tính toán các hash
+PKG_MD5=$(md5sum ./Packages | cut -d ' ' -f 1)
+PKG_SIZE=$(stat ./Packages --printf="%s")
+PKG_BZ2_MD5=$(md5sum ./Packages.bz2 | cut -d ' ' -f 1)
+PKG_BZ2_SIZE=$(stat ./Packages.bz2 --printf="%s")
+PKG_GZ_MD5=$(md5sum ./Packages.gz | cut -d ' ' -f 1)
+PKG_GZ_SIZE=$(stat ./Packages.gz --printf="%s")
+
+PKG_SHA256=$(sha256sum ./Packages | cut -d ' ' -f 1)
+PKG_BZ2_SHA256=$(sha256sum ./Packages.bz2 | cut -d ' ' -f 1)
+PKG_GZ_SHA256=$(sha256sum ./Packages.gz | cut -d ' ' -f 1)
+
+# Tạo file Release
+cat > Release << EOF
+Origin: Kho Lưu Trữ Romlayvn
+Label: Kho Tinh Chỉnh iOS
+Suite: stable
+Version: 2.0
+Codename: ios
+Architectures: iphoneos-arm64 iphoneos-arm64e
+Components: main
+Description: Kho Tinh Chỉnh iOS Hiện Đại
+MD5Sum:
+ $PKG_MD5 $PKG_SIZE Packages
+ $PKG_BZ2_MD5 $PKG_BZ2_SIZE Packages.bz2
+ $PKG_GZ_MD5 $PKG_GZ_SIZE Packages.gz
+SHA256:
+ $PKG_SHA256 $PKG_SIZE Packages
+ $PKG_BZ2_SHA256 $PKG_BZ2_SIZE Packages.bz2
+ $PKG_GZ_SHA256 $PKG_GZ_SIZE Packages.gz
+EOF
 
 echo "------------------"
 echo "Done!"
